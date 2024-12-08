d8f5733b134780fe736c74589cf0e837d9b38ff2
whdev36 <wisdomhunter36@gmail.com>
2024-12-07 22:38:54 -0800

    xabarlar yaratildi

--- talaba/views.py
+++ talaba/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from .forms import KirishForm, FROForm
+from django.contrib import messages as m
 
 
 # Bosh sahifa
@@ -11,27 +12,39 @@ def uy(r):
 def kirish(r):
 	if r.method == 'POST': # Foydalanuvchi ma'lumot yuborgan bo'lsa
 		form = KirishForm(r.POST) # Formani POST ma'lumotlari bilan to'ldirish
-		if form.is_valid(): # Forma validatsiyadan o‘tdimi?
+		if form.is_valid(): # Forma validatsiyadan o'tdimi?
 			f_nomi = form.cleaned_data.get('f_nomi') # Foydalanuvchi nomini olish
 			parol = form.cleaned_data.get('parol') # Parolni olish
 			f = authenticate(username=f_nomi, password=parol) # Autentifikatsiya qilish
 			if f is not None: # Agar foydalanuvchi mavjud bo'lsa
 				login(r, f) # Foydalanuvchini tizimga kiritish
+				m.success(r, 'Tizimga muvaffaqiyatli kirdingiz.') # Muvaffaqiyatli xabar
 				return redirect('uy') # Muvaffaqiyatli login bo'lsa, asosiy sahifaga yo'naltirish
 			else:
+				m.error(r, 'Foydalanuvchi nomi yoki parol noto\'g\'ri.') # Xatolik xabari
 				form.add_error(None, "Foydalanuvchi nomi yoki parol noto'g'ri.")  # Xatolik qo'shish
+		else:
+			m.warning('Forma to\'g\'ri to\'ldirilmagan.') # Forma xatoligi haqida ogohlantirish
 	else:
 		form = KirishForm() # GET so‘rovda bo‘sh forma yuborish
 
 	return render(r, 'kirish.html', {'form': form}) # Formani sahifaga uzatish
 
 def ro(r):
+	"""
+    Ro'yxatdan o'tish ko'rinishi.
+    Foydalanuvchi muvaffaqiyatli ro'yxatdan o'tganda tizimga kiradi va 
+    xabar orqali muvaffaqiyatli ro'yxatdan o'tganligini bildiradi.
+    """
 	if r.method == 'POST':  # Foydalanuvchi ma'lumot yuborgan bo'lsa
 		form = FROForm(r.POST)  # Formani foydalanuvchi yuborgan ma'lumotlar bilan to'ldirish
 		if form.is_valid():  # Forma validatsiyadan o'tdimi?
 			f = form.save()  # Yangi foydalanuvchini yaratish va saqlash
 			login(r, f)  # Yangi foydalanuvchini tizimga kiritish
+			m.success(r, 'Muvaffaqiyatli ro\'yxatdan o\'tdingiz!')
 			return redirect('uy')  # Muvaffaqiyatli ro'yxatdan o'tgach, asosiy sahifaga yo'naltirish
+		else:
+			m.error(r, 'Ro\'yxatdan o\'tishda xatolik yuz berdi. Iltimos, formani to\'g\'ri to\'ldiring.')
 	else:
 		form = FROForm()  # GET so'rovda bo'sh forma yaratish
 
@@ -39,5 +52,11 @@ def ro(r):
 	return render(r, 'ro.html', {'form': form})
 
 def chiqish(r):
-	logout(r)
-	return redirect('uy') # Bosh safihaga yo'naltirish
+    """
+    Foydalanuvchini tizimdan chiqarish.
+    Chiqib ketgandan so'ng foydalanuvchini bosh sahifaga yo'naltiradi
+    va xabar orqali muvaffaqiyatli chiqganligini bildiradi.
+    """
+    logout(r)  # Foydalanuvchini tizimdan chiqarish
+    m.success(r, "Tizimdan muvaffaqiyatli chiqdingiz.")  # Xabar qo'shish
+    return redirect('uy')  # Bosh sahifaga yo'naltirish
